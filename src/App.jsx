51517eabf6ca4cd8e65ab894ddd434a98aa4e23b
abdulrehman751 <abdulrehmanashfaq294@gmail.com>
2024-11-11 22:12:24 +0500

    change routing integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,27 @@
 import HomePage from "./routes/homePage/homePage";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 import ListPage from "./routes/listPage/listPage";
 import Layout from "./routes/layout/layout";
 import SinglePage from "./routes/singlePage";
 import ProfilePage from "./routes/profilePage/profilePage";
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          path: "/",
-          element: <HomePage />,
-        },
-        {
-          path: "/list",
-          element: <ListPage />,
-        },
-        {
-          path: "/:id",
-          element: <SinglePage />,
-        },
-        {
-          path: "/profile",
-          element: <ProfilePage />,
-        },
-      ],
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/list' element={<ListPage />} />
+          <Route path='/:id' element={<SinglePage />} />
+          <Route path='/profile' element={<ProfilePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
