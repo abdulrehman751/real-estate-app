3a64bfe956c11ba63857ca322c2db8d4c9708dba
abdulrehman751 <abdulrehmanashfaq294@gmail.com>
2024-11-11 22:37:32 +0500

    fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import HomePage from "./routes/homePage/homePage";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  BrowserRouter,
-} from "react-router-dom";
 import ListPage from "./routes/listPage/listPage";
-import Layout from "./routes/layout/layout";
 import SinglePage from "./routes/singlePage";
 import ProfilePage from "./routes/profilePage/profilePage";
+import Layout from "./routes/layout/layout";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Layout />}>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/list' element={<ListPage />} />
-          <Route path='/:id' element={<SinglePage />} />
-          <Route path='/profile' element={<ProfilePage />} />
+          <Route index element={<HomePage />} /> {/* Default route */}
+          <Route path='list' element={<ListPage />} />
+          <Route path=':id' element={<SinglePage />} />
+          <Route path='profile' element={<ProfilePage />} />
         </Route>
       </Routes>
     </BrowserRouter>
